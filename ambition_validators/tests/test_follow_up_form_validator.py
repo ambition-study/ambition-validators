b6581df2c9568a15a1f8cea53cb9458459fd8841
Leslie <leslie.kep@gmail.com>
2018-06-22 15:31:03 +0200

    add blood transfusion validator

--- ambition_validators/tests/test_follow_up_form_validator.py
+++ ambition_validators/tests/test_follow_up_form_validator.py
@@ -46,6 +46,20 @@ class TestFollowUpFormValidator(TestCase):
         self.assertRaises(ValidationError, form_validator.validate)
         self.assertIn('fluconazole_dose_other', form_validator._errors)
 
+    def test_blood_transfusions_blood_yes_transfusions_units(self):
+        cleaned_data = {'blood_transfusions': YES,
+                        'blood_transfusions_units': None}
+        form_validator = FollowUpFormValidator(cleaned_data=cleaned_data)
+        self.assertRaises(ValidationError, form_validator.validate)
+        self.assertIn('blood_transfusions_units', form_validator._errors)
+
+    def test_blood_transfusions_blood_no_transfusions_units(self):
+        cleaned_data = {'blood_transfusions': NO,
+                        'blood_transfusions_units': 30}
+        form_validator = FollowUpFormValidator(cleaned_data=cleaned_data)
+        self.assertRaises(ValidationError, form_validator.validate)
+        self.assertIn('blood_transfusions_units', form_validator._errors)
+
 #     def test_care_before_hospital_yes(self):
 #         cleaned_data = {'care_before_hospital': YES,
 #                         'location_care': NOT_APPLICABLE}
